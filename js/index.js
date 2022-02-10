cc9f4ac64d0e6324c3ac1fa891c12267d900f077
Rodrigo Miguel Arede <aredemiguel@gmail.com>
2022-02-10 22:22:36 +0000

    made a slider out of about_me section

---
+++ js/index.js
@@ -0,0 +1,26 @@
+const hobbies_slide = document.querySelector('.hobbies_slide');
+const hobbies = document.querySelectorAll('.hobby');
+
+// buttons
+const prevBtn = document.querySelector('#prev_hobby');
+const nextBtn = document.querySelector('#next_hobby');
+
+// counter
+let counter = 0;
+
+// button listeners
+
+nextBtn.addEventListener('click', function(){
+    if (counter < hobbies.length - 1) counter++;
+    else counter = 0;
+    hobbies_slide.style.transform = 'translateX(' + (-hobbies[0].clientWidth*counter) + 'px)';
+});
+
+prevBtn.addEventListener('click', function(){
+    if (counter > 0) counter--;
+    else counter = hobbies.length - 1;
+    hobbies_slide.style.transform = 'translateX(' + (-hobbies[0].clientWidth*counter) + 'px)';
+});
+
+
+
